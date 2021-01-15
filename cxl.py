b93b947548329eaf31cf340a837233258fffe87f
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2021-01-15 19:58:46 +0800

    update

---
+++ cxl.py
@@ -0,0 +1,52 @@
+import requests,time
+with open('配置文件.txt','r',encoding="utf-8") as f:
+   pzwj=f.read()  #文件的读操作
+#pzwj=pzwj.replace('\n', '').replace('\r', '')
+#pzwj=pzwj.replace(' ', '')
+qq=pzwj.split('\n')[2]
+print(qq)
+city1=pzwj.split('\n')[1]
+print(city1)
+def getToday(cityName):
+    url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityName
+    response = requests.get(url)
+    weatherDict = response.json()
+    print(weatherDict)
+    if weatherDict['desc'] == 'OK':
+        print('你输入的城市是正确的')
+        # 获取城市名
+        global city, wendu,month,forecast
+        city = weatherDict['data']['city']
+        print('城市名称', city)
+        # 获取当前温度
+        wendu = weatherDict['data']['wendu'] + '℃ '
+        # 获取月份
+        month = time.strftime('%m')
+        print(month)
+        forecast = weatherDict['data']['forecast']
+        # 获取日期
+        global date, type, high, low, data
+        date = month + '月' + forecast[0]['date']
+        # 获取天气类型
+        type = forecast[0]['type']
+        # 获取最高温度
+        high = forecast[0]['high']
+        # 获取最低温度
+        low = forecast[0]['low']
+        ganmao=weatherDict['data']['ganmao']
+        
+        url = 'https://api.uixsj.cn/hitokoto/get?type=hitokoto&code=json'
+        response = requests.get(url)
+        weatherDict = response.json()
+        print(weatherDict)
+        content=weatherDict["type"]+"："+weatherDict['content']
+        
+        data={'msg':date+"\n"+city+"当前温度为："+wendu+"\n"+"当前天气:"+type+"\n"+"最"+high+"\n"+"最"+low+"\n"+ganmao+"\n"+content,
+              'qq':qq}
+    else:
+        print('你输入的城市是错误的')
+getToday(city1)
+
+url='https://qmsg.zendee.cn/send/'+pzwj.split('\n')[0]
+r=requests.post(url,data=data)
+print(r.text)
---
+++ 上传同步（请勿点击）.bat
@@ -0,0 +1,5 @@
+git init
+git add .              #将文件夹内所有文件添加至本地仓库
+git commit -m "update"           #添加本次提交描述
+git remote add origin https://github.com/cxl2020MC/weather-helper.git
+git push -u origin master
