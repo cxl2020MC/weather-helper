a6368f865976a1c09c4c3188aee0af776fa99805
cxl2020MC <69844153+cxl2020MC@users.noreply.github.com>
2021-01-16 11:54:04 +0800

    Add files via upload

--- cxl.py
+++ cxl.py
@@ -4,9 +4,13 @@ with open('配置文件.txt','r',encoding="utf-8") as f:
 #pzwj=pzwj.replace('\n', '').replace('\r', '')
 #pzwj=pzwj.replace(' ', '')
 qq=pzwj.split('\n')[2]
-print(qq)
+#print(qq)
 city1=pzwj.split('\n')[1]
-print(city1)
+#print(city1)
+apikey=pzwj.split('\n')[0]
+#print(apikey)
+xzqdapi=pzwj.split('\n')[3]
+print('qq:'+qq+','+'city:'+city1+','+'apikey:'+apikey+'你选择的api序号：'+xzqdapi)
 def getToday(cityName):
     url = 'http://wthrcdn.etouch.cn/weather_mini?city=' + cityName
     response = requests.get(url)
@@ -22,7 +26,7 @@ def getToday(cityName):
         wendu = weatherDict['data']['wendu'] + '℃ '
         # 获取月份
         month = time.strftime('%m')
-        print(month)
+        #print(month)
         forecast = weatherDict['data']['forecast']
         # 获取日期
         global date, type, high, low, data
@@ -34,19 +38,29 @@ def getToday(cityName):
         # 获取最低温度
         low = forecast[0]['low']
         ganmao=weatherDict['data']['ganmao']
-        
-        url = 'https://api.uixsj.cn/hitokoto/get?type=hitokoto&code=json'
-        response = requests.get(url)
-        weatherDict = response.json()
-        print(weatherDict)
-        content=weatherDict["type"]+"："+weatherDict['content']
-        
-        data={'msg':date+"\n"+city+"当前温度为："+wendu+"\n"+"当前天气:"+type+"\n"+"最"+high+"\n"+"最"+low+"\n"+ganmao+"\n"+content,
+        # 获取风向
+        fx=weatherDict['data']['yesterday']['fx']
+        if int(xzqdapi) == 1:
+            url1 = 'https://api.uixsj.cn/hitokoto/get?type=hitokoto&code=json'
+            apiget = requests.get(url1)
+            apidata = apiget.json()
+            print(apidata)
+            content=apidata["type"]+"："+apidata['content']
+        else:
+            url1 = 'https://v1.hitokoto.cn/?c=f&encode=text'
+            apiget = requests.get(url1)
+            apidata = apiget.text
+            content='一言:'+str(apidata)
+            print(content)
+        data={'msg':date+"\n"+city+"当前温度为："+wendu+"\n"+"当前天气:"+type+"\n"+fx+"\n"+"最"+high+"\n"+"最"+low+"\n"+ganmao+"\n"+content,
               'qq':qq}
+        print('发送预览：'+'\n'+data['msg'])
     else:
         print('你输入的城市是错误的')
 getToday(city1)
+#getToday('龙港区')
 
-url='https://qmsg.zendee.cn/send/'+pzwj.split('\n')[0]
+url='https://qmsg.zendee.cn/send/'+apikey
+print(url)
 r=requests.post(url,data=data)
 print(r.text)
